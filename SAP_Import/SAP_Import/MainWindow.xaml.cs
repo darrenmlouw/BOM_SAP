ffb0487bcc474086c683d327cc2160c0d3f6f503
Darren <darrenmlouw@gmail.com>
2022-01-13 16:30:39 +0200

    End Day

--- SAP_Import/SAP_Import/MainWindow.xaml.cs
+++ SAP_Import/SAP_Import/MainWindow.xaml.cs
@@ -16,16 +16,22 @@ using System.Windows.Shapes;
 using System.Runtime.InteropServices;
 using Excel = Microsoft.Office.Interop.Excel;
 
+using forms = System.Windows.Forms;
+
+
+using Microsoft.Win32;
+
 
 
 using PostSharp.Patterns.Threading;
 
 namespace SAP_Import
 {
-    public class ExcelClass
+    public class ExcelRead
     {
+        public string filename = "";
         public string path = "";
-        public Excel.Application excel = new Excel.Application();
+        public Excel.Application excel;
         public Excel.Workbook wb;
         public Excel.Worksheet ws;
         public Excel.Range range;
@@ -34,17 +40,20 @@ namespace SAP_Import
         public int cols;
 
 
-        public ExcelClass(string path, int sheet)
+        public ExcelRead(string path, int sheet, string name)
         {
             try
             {
+                this.filename = name;
                 this.path = path;
-                wb = excel.Workbooks.Open(path);
-                ws = wb.Worksheets[sheet];
 
-                range = ws.UsedRange;
-                rows = range.Rows.Count;
-                cols = range.Columns.Count;
+                this.excel = new Excel.Application();                
+                this.wb = excel.Workbooks.Open(path);
+                this.ws = wb.Worksheets[sheet];
+                this.range = ws.UsedRange;
+
+                this.rows = range.Rows.Count;
+                this.cols = range.Columns.Count;
             }
             catch
             {
@@ -69,18 +78,36 @@ namespace SAP_Import
 
         public void Release()
         {
-            Marshal.ReleaseComObject(ws);
-            Marshal.ReleaseComObject(wb);
-            Marshal.ReleaseComObject(excel);
+            //excel.Workbooks.Close();
+            //wb.Close(true, System.Reflection.Missing.Value, System.Reflection.Missing.Value);
+            this.excel.Quit();
+
+            Marshal.ReleaseComObject(this.ws);
+            Marshal.ReleaseComObject(this.wb);
+            Marshal.ReleaseComObject(this.excel);
         }
     }
+
+    public class ExcelCreate
+    {
+        Excel.Application xlApp;
+        Excel.Workbook xlWorkBook;
+        Excel.Worksheet xlWorkSheet;
+        object misValue = System.Reflection.Missing.Value;
+
+        public ExcelCreate()
+        {
+            xlApp = new Microsoft.Office.Interop.Excel.Application();
+        }
+    }
+
 
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : System.Windows.Window
     {
-        ExcelClass ImportExcelFile;
+        ExcelRead ImportExcelFile;
         bool hasFile = false;
 
         public MainWindow()
@@ -90,12 +117,12 @@ namespace SAP_Import
 
         private void DragBlock_DragOver(object sender, DragEventArgs e)
         {
-            DragBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromRgb(200, 200, 255));
+            DragBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromArgb(153, 200, 200, 255));
         }
 
         private void DragBlock_DragLeave(object sender, DragEventArgs e)
         {
-            DragBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromRgb(200, 200, 200));
+            DragBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromArgb(153, 200, 200, 200));
         }
 
         [Background]
@@ -105,7 +132,7 @@ namespace SAP_Import
             hasFile = false;
             Dispatcher.BeginInvoke((Action)(() =>
             {
-                Convert.IsEnabled = false;
+                Convert_Back.IsEnabled = false;
             }));
 
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
@@ -115,31 +142,51 @@ namespace SAP_Import
                 string path = System.IO.Path.GetFullPath(files[0]);
                 string extension = System.IO.Path.GetExtension(files[0]);
 
-                Dispatcher.BeginInvoke((Action)(() =>
-                {
-                    DragBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromRgb(200, 255, 200));
-                    Filename.Text = filename + "\n\n" + extension;
-                }));
-
+                
                 if (extension == ".xlsx")
                 {
-                    ImportExcelFile = new ExcelClass(path, 1);
+                    ImportExcelFile = new ExcelRead(path, 1, filename);
                     hasFile = true;
+
+                    Dispatcher.Invoke((Action)(() =>
+                    {
+                        ConsoleWindow.Children.Clear();
+
+                        DragBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromArgb(153, 200, 255, 200));
+                        Filename.Text = filename + "\n\n" + extension;
+
+                        print("Excel File Information:", 12, "bold");
+                        print("line", 0, "");
+                        print("Rows: " + ImportExcelFile.rows, 12, "");
+                        print("Cols: " + ImportExcelFile.cols, 12, "");
+                        print("line", 0, "");
+                    }));
+
+                    
                     Dispatcher.BeginInvoke((Action)(() =>
                     {
-                        Convert.IsEnabled = true;
+                        Convert_Back.IsEnabled = true;
                     }));
                 }
                 else
                 {
                     MessageBox.Show("File Does Not Have .xlsx Extension", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Dispatcher.BeginInvoke((Action)(() =>
+                    {
+                        ConsoleWindow.Children.Clear();
+                        DragBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromArgb(153, 255, 200, 200));
+                        Filename.Text = "Drag .XLSX File";
+
+                        
+                    }));
                 }
             }
             else
             {
                 Dispatcher.BeginInvoke((Action)(() =>
                 {
-                    DragBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromRgb(255, 200, 200));
+                    ConsoleWindow.Children.Clear();
+                    DragBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromArgb(153, 255, 200, 200));
                 }));
             }
 
@@ -154,7 +201,7 @@ namespace SAP_Import
             {
                 Dispatcher.BeginInvoke((Action)(() =>
                 {
-                    Convert.IsEnabled = false;
+                    Convert_Back.IsEnabled = false;
                 }));
                 ProgressStart();
 
@@ -163,24 +210,160 @@ namespace SAP_Import
                 Console.WriteLine("Cols: " + ImportExcelFile.cols.ToString());
                 Console.WriteLine("------------------------------------------------------------");
 
-                for (int i = 0; i < ImportExcelFile.rows; i++)
+                //for (int i = 0; i < ImportExcelFile.rows; i++)
+                //{
+                //    for (int j = 0; j < ImportExcelFile.cols; j++)
+                //    {
+                //        Console.Write(ImportExcelFile.ReadCell(i, j) + "|");
+                //    }
+                //    Console.WriteLine();
+                //}
+
+
+                bool isConverted;
+                try
                 {
-                    for (int j = 0; j < ImportExcelFile.cols; j++)
-                    {
-                        Console.Write(ImportExcelFile.ReadCell(i, j) + "|");
-                    }
-                    Console.WriteLine();
+                    ConvertBOM();
+
+                    ImportExcelFile.Release();
+                    isConverted = true;
+                }
+                catch
+                {
+                    isConverted = false; ;
                 }
 
                 ImportExcelFile.Release();
 
                 Console.WriteLine("------------------------------------------------------------");
 
+                Dispatcher.Invoke((Action)(() =>
+                {
+                    Filename.Text = "Drag .XLSX File";
+
+                    if (isConverted == true)
+                    {
+                        Status.Text = "Success";
+                        OutBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromArgb(153, 200, 255, 200));
+                    }
+                    else
+                    {
+                        Status.Text = "Failed";
+                        OutBlock.Background = new System.Windows.Media.SolidColorBrush(Color.FromArgb(153, 255, 200, 200));
+                    }
+                }));
+
                 ProgressEnd();
             }
             else
             {
                 MessageBox.Show("No File Selected", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        [Background]
+        private void ConvertBOM()
+        {
+            print("Begnning Conversion:", 12, "bold");
+            print("line", 0, "");
+
+            //CreateBOMs();
+        }
+
+        [Background]
+        private void CreateBOMs()
+        {
+            Excel.Application xlApp = new Microsoft.Office.Interop.Excel.Application();
+
+            if (xlApp == null)
+            {
+                MessageBox.Show("Excel is not properly installed!!");
+            }
+
+
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlWorkBook = xlApp.Workbooks.Add(misValue);
+            //xlWorkBook.Worksheets.Add
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+
+            xlWorkSheet.Cells[1, 1] = "ID";
+            xlWorkSheet.Cells[1, 2] = "Name";
+            xlWorkSheet.Cells[2, 1] = "1";
+            xlWorkSheet.Cells[2, 2] = "One";
+            xlWorkSheet.Cells[3, 1] = "2";
+            xlWorkSheet.Cells[3, 2] = "Two";
+
+            string savePath = "";
+
+            savePath = ImportExcelFile.path;
+
+            // Keep This Code To Select Folder To Save IN
+            //Dispatcher.Invoke((Action)(() =>
+            //{
+            //    forms.FolderBrowserDialog openFileDialog = new forms.FolderBrowserDialog();
+
+            //    if (openFileDialog.ShowDialog() == forms.DialogResult.OK)
+            //    {
+            //        savePath = openFileDialog.SelectedPath;
+            //    }
+            //}));
+
+            //txtEditor.Text = File.ReadAllText(openFileDialog.FileName);
+            //savePath = Path.Get
+
+            string temp = savePath.Replace(ImportExcelFile.filename, "BOMS.csv");
+
+            Console.WriteLine(temp);
+            print(temp, 14, "");
+
+            try
+            {
+                xlWorkBook.SaveAs(temp);
+            }
+            catch
+            {
+
+            }
+            xlWorkBook.Close(true, misValue, misValue);
+            xlApp.Quit();
+
+            Marshal.ReleaseComObject(xlWorkSheet);
+            Marshal.ReleaseComObject(xlWorkBook);
+            Marshal.ReleaseComObject(xlApp);
+        }
+
+        [Dispatched]
+        private void print(string text, double size, string weight)
+        {
+            if (text == "line")
+            {
+                Border line = new Border();
+                line.Height = 1;
+                line.Background = new System.Windows.Media.SolidColorBrush(Color.FromRgb(150, 150, 150));
+                line.Margin = new Thickness(0, 1, 0, 1);
+                line.CornerRadius = new CornerRadius(0);
+
+                ConsoleWindow.Children.Add(line);
+            }
+            else
+            {
+
+                TextBlock tb = new TextBlock();
+                tb.Text = text;
+                tb.FontSize = size;
+                if (weight == "bold")
+                {
+                    tb.FontWeight = FontWeights.Bold;
+                }
+                else
+                {
+                    tb.FontWeight = FontWeights.Regular;
+                }
+
+                ConsoleWindow.Children.Add(tb);
             }
         }
 
